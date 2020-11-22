5a533651044a43efb9495f86b78f42bdb59dc1ac
Devyn Bailey <devynbailey11@yahoo.com>
2020-11-21 18:39:00 -0700

    fixed bug for readme to write, added badges

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# README_generator
+
+  # Application Title: repo
+
+  ## Application Discription: repo
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [Test](#test)
+  * [License](#license)
+  * [Questions](#questions)
+ 
+  ## Installation 
+  user
+
+  ## Usage
+  user
+
+  ## Contributing
+  user
+
+  ## Test
+  user
+
+  ## License
+  [![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
+    
+  
+
+
+  ## Questions
+  * Find me on GitHub: db
+  * Email me: @
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,3 +1,32 @@
+
+//function to call specific license badges
+//is there a way to loop through the choices and call the badges?
+function renderLicenseBadge(license) {
+  if (license === "Apache") {
+    return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+    `;
+  } else if (license === "Academic") {
+    return `[![Generic badge](https://img.shields.io/badge/<SUBJECT>-<ACADEMIC>-<COLOR>.svg)](https://shields.io/)
+    `;
+  } else if (license === "MIT") {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+    `;
+  } else if (license === "Mozilla") {
+    return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
+    `;
+  } else if (license === "GNUGPLv3") {
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+    `;
+  } else if (license === "ISC") {
+    return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)
+    `;
+  } else if (license === "Open") {
+    return `[![License: ODbL](https://img.shields.io/badge/License-ODbL-brightgreen.svg)](https://opendatacommons.org/licenses/odbl/)
+    `;
+  }
+  return "";
+}
+
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `
@@ -26,12 +55,16 @@ function generateMarkdown(data) {
   ${data.test}
 
   ## License
-  ${data.license}
+  ${renderLicenseBadge(data.license)}
+  
+
 
   ## Questions
   * Find me on GitHub: ${data.username}
   * Email me: ${data.email}
 `;
 }
+
+console.log("hi", renderLicenseBadge);
 
 module.exports = generateMarkdown;
